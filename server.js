d0f8cad6f4dfd39fca7c2107895d5f83b78ca4bf
wasantac <34143200+wasantac@users.noreply.github.com>
2021-07-24 01:16:36 -0500

    update port

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const io = require('socket.io')(3001)
+const io = require('socket.io')(process.env.PORT || 3001,{
+    cors: {
+        origin: "*"
+    }
+})
 
 io.on("connection",socket =>{
     socket.on("send-data",data => {
