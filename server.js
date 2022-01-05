e357ba77315c6c845106565db56e9829907449c6
wasantac <34143200+wasantac@users.noreply.github.com>
2022-01-05 01:25:59 -0500

    nuevo socket

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const io = require('socket.io')(process.env.PORT || 3001, {
     }
 })
 
-io.on("connection",socket =>{
-    socket.on("open-points",data =>{
-        socket.broadcast.emit("send-animation",data)
+io.on("connection", socket => {
+    socket.on("send-request", data => {
+        socket.broadcast.emit("receive-request", data)
         console.log(data)
     })
-    socket.on("send-data",data => {
-        socket.broadcast.emit("receive-data",data);
+    socket.on("send-request-marcador", data => {
+        socket.broadcast.emit("receive-request-marcador", data)
+        console.log(data)
+    })
+    socket.on("open-points", data => {
+        socket.broadcast.emit("send-animation", data)
+        console.log(data)
+    })
+    socket.on("send-data", data => {
+        socket.broadcast.emit("receive-data", data);
         console.log(data)
     })
     socket.on("rey-colina", data => {
@@ -21,5 +29,6 @@ io.on("connection",socket =>{
         socket.broadcast.emit("ganador", data);
         console.log(data)
     })
+
     console.log("conected")
 })
